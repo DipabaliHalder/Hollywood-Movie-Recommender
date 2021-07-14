0026650379fd6fe382100ce1ea8f8a64ed82c247
Dipabali_Halder <85602238+DipabaliHalder@users.noreply.github.com>
2021-07-14 20:47:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def recommend(movie):
 
 st.header('**Movie Recommendation System**')
 movies = pickle.load(open('movie_list.pkl','rb'))
-similarity = pickle.load(open('similarity.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl','rb+'))
 
 movie_list = np.sort(movies['title'].unique())
 movie_list = np.insert(movie_list,0,'')
